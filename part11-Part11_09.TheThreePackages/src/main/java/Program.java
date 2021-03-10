d9c614a524dbf81772a5d7f73ad084bba0d5e260
Miguel-Barca <miguelbarca.usc@gmail.com>
2021-03-10 13:10:26 +0000

    Packages exercises

---
+++ part11-Part11_09.TheThreePackages/src/main/java/Program.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // you can test your classes here
+
+    }
+}
