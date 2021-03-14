0c1e84117d50ee919757026ffc1632afd56d5605
Miguel-Barca <miguelbarca.usc@gmail.com>
2021-03-14 13:50:58 +0000

    Magic square. Sum of principal and secondary diagonals. Siamese method of creating magic squares

---
+++ part12-Part12_10.MagicSquare/src/main/java/Program.java
@@ -0,0 +1,11 @@
+
+public class Program {
+
+    public static void main(String[] args) {
+        // Test the MagicSquare class here
+        
+        MagicSquareFactory ms = new MagicSquareFactory();
+        System.out.println(ms.createMagicSquare(2));
+      
+    }
+}
